f715e8cdf27ec521865c6c3644882dd5c12f6ea1
Danushnan <dankri6455@gmail.com>
2025-02-20 20:08:34 +0000

    Update Fox.java

--- Fox.java
+++ Fox.java
@@ -15,7 +15,7 @@ import javafx.scene.paint.Color;
 public class Fox extends Animal {
 
     private static final int BREEDING_AGE = 15;
-    private static final int MAX_AGE = 120;
+    private static final int MAX_AGE = 40;
     private static final double BREEDING_PROBABILITY = 0.08;
     private static final int MAX_LITTER_SIZE = 2;
     private static final int RABBIT_FOOD_VALUE = 9;
